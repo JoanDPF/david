653b910d75016726211709ca0b210de66676e191
Joan Palomino <joanpalomino19@gmail.com>
2020-07-09 18:58:01 -0500

    Pregunta02- Listo

---
+++ Preg02.py
@@ -0,0 +1,8 @@
+#Autor: Joan Palomino Salas_estudiante02
+
+print("Validadcion de Correo")
+correo = input("Ingrese Correo: ")
+if "@untels.edu.pe" in correo:
+	print("Si es correo de la UNTELS")
+else:
+	print("No es correo de la UNTELS " )
